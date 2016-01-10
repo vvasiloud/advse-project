eca86f57034210aac9a0080293c6409072dfb4e2
Chris Givanoudis <chgivan@gmail.com>
2016-01-10 11:34:05 +0200

    Unit Test

--- src/test/java/gr/etherasTickets/flight/FlightTest.java
+++ src/test/java/gr/etherasTickets/flight/FlightTest.java
@@ -3,13 +3,19 @@ package gr.etherasTickets.flight;
 import java.util.Date;
 import java.util.Random;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.support.AnnotationConfigContextLoader;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import static org.hamcrest.Matchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -19,20 +25,29 @@ import gr.etherasTickets.EtherasTicketsApplication;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = EtherasTicketsApplication.class, loader = AnnotationConfigContextLoader.class)
+@WebAppConfiguration
 public class FlightTest {
 	
 	private final String citiesPool[] = {"Thessaloniki" , "Ioannina" , "Athens" , "Patras" , "Heraklion" , "Rhodes" , "Larissa" , "Volos"};
 	private final Integer pricePool[] = {20 , 30 , 50 , 80 , 100 , 150 , 200 , 300};
 	private final Integer seatPool[] = {40 , 50 , 60 , 70 , 100};
 	private final Random random = new Random();
+	
 	private MockMvc mockMvc;
 	
+	@Autowired
+    private WebApplicationContext webApplicationContext;
 	
 	@Autowired
 	private FlightRepository repository;
 	
-	@Test
-	public void testCreateRandomFlights(){
+	@Before
+	public void SetUp(){
+		 mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+	}
+	
+	@Before
+	public void setUpRandomFlights(){
 		int flightsSize = 50;
 		Flight[] flights = new Flight[flightsSize];
 		
@@ -64,12 +79,8 @@ public class FlightTest {
 			
 			mockMvc.perform(get("/flights").param("maxPrice", "50").param("minPrice","30"))
 			.andExpect(status().isOk())
-			.andExpect(jsonPath("$[0].price", gte(1)));
+			.andExpect((ResultMatcher) jsonPath("$[0]" , Criteria.where("price").gte("50").and("price").lte("30")));
 			
-			
-		
-			
-		
 		
 		
 		
