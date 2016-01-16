76cb0fb90013b4ef42693725ac480f3be78ab8d7
vgeorga <zeitsef@gmail.com>
2016-01-16 13:46:35 +0200

    ReservationRepositoryImpl added the the mongooperations attr and initialisation

--- src/main/java/gr/etherasTickets/user/ReservationRepositoryImpl.java
+++ src/main/java/gr/etherasTickets/user/ReservationRepositoryImpl.java
@@ -1,6 +1,17 @@
 package gr.etherasTickets.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.util.Assert;
+
 public class ReservationRepositoryImpl implements CustomReservationRepository {
 
+	 private final MongoOperations operations; //Class for communicating with mongoDB
+
+	    @Autowired
+	    public ReservationRepositoryImpl(MongoOperations operations) {
+	        Assert.notNull(operations, "MongoOperations must not be null!");
+	        this.operations = operations;
+	    }
 
 }
