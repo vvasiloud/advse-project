a9778fe870ebcabbe0a22e8f209410a2b721d13c
Konstantara Konstantina <mai16013@uom.edu.gr>
2016-01-17 23:12:53 +0200

    Ionic proxy setup

--- client/etheras/gulpfile.js
+++ client/etheras/gulpfile.js
@@ -6,6 +6,8 @@ var sass = require('gulp-sass');
 var minifyCss = require('gulp-minify-css');
 var rename = require('gulp-rename');
 var sh = require('shelljs');
+var replace = require('replace');
+var replaceFiles = ['./www/js/app.js'];
 
 var paths = {
   sass: ['./scss/**/*.scss']
@@ -48,4 +50,24 @@ gulp.task('git-check', function(done) {
     process.exit(1);
   }
   done();
+})
+
+gulp.task('add-proxy', function() {
+  return replace({
+    regex: "http://cors.api.com/api",
+    replacement: "http://localhost:8100/api",
+    paths: replaceFiles,
+    recursive: false,
+    silent: false,
+  });
+})
+
+gulp.task('remove-proxy', function() {
+  return replace({
+    regex: "http://localhost:8100/api",
+    replacement: "http://cors.api.com/api",
+    paths: replaceFiles,
+    recursive: false,
+    silent: false,
+  });
 });
